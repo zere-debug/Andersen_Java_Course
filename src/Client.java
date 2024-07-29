d968151cf7235e8e8a1b008c10be57d830939cc4
Zere Kairatova <kairatova.z@mail.ru>
2024-07-30 00:15:37 +0500

    Second commit

---
+++ src/Client.java
@@ -0,0 +1,16 @@
+public class User extends Ticket{
+    private String name;
+    public String surName;
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public void getTicket(){
+        
+    }
+
+}
+
